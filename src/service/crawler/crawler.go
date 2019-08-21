0698f3831a2402cfef4d27bb9470aad9380b22d5
Mingfei Ding <xiesansi@gmail.com>
2019-08-22 00:24:05 +0800

    directory structure adjustment

---
+++ src/service/crawler/crawler.go
@@ -0,0 +1,6 @@
+package crawler
+
+// Run crawler run
+func Run() {
+
+}
---
+++ src/service/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/hanbaowang/LogViewer/src/service/server"
+
+func main() {
+	server.Serve()
+}
