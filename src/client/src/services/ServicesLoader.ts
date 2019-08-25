933867fbd1b97701cc02399134234129bffd662e
Mingfei Ding <xiesansi@gmail.com>
2019-08-26 00:47:35 +0800

    revert a bug caused by accident

--- src/client/src/services/ServicesLoader.ts
+++ src/client/src/services/ServicesLoader.ts
@@ -7,6 +7,6 @@ import Ajax from "../axios/Ajax";
 export default async function getServices() {
   let services: Service[] = await Ajax.get<Service[]>(URLS.SERVICES)
     .then(resp => resp.data)
-    // .catch(resp => config.getServicesResp);
+    .catch(resp => config.getServicesResp);
   return services;
 }
